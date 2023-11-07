ddd21059041ad9513e1b38a640f44cc6697a40a5
rhythmjayee <jayeerythm8@gmail.com>
2023-11-07 19:02:31 +0530

    connect DB to product service

--- productservice/src/main/java/com/productservice/productservice/repository/Product/ProductRepository.java
+++ productservice/src/main/java/com/productservice/productservice/repository/Product/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.productservice.productservice.repository.Product;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.productservice.productservice.model.Product;
 
 public interface ProductRepository extends MongoRepository<Product, String>{
+    @Query(value="{sellerID : ?0}")    
+    List<Product> findAllProductsBySellerID(String sellerID);
 }
--- productservice/src/main/java/com/productservice/productservice/service/ProductService.java
+++ productservice/src/main/java/com/productservice/productservice/service/ProductService.java
@@ -1,8 +1,56 @@
 package com.productservice.productservice.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.productservice.productservice.model.Product;
+import com.productservice.productservice.repository.Product.ProductRepository;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product newProduct) {
+        Product product = new Product(
+                    newProduct.getSellerID(), newProduct.getProductName(),
+                    newProduct.getProductDescription(), newProduct.getProductQuantity());
+        productRepository.save(product);
+        return product;
+    }
+    public Product updateProduct(String productID, Product newProduct) {
+        Optional<Product> dbRes = productRepository.findById(productID);
+        if(!dbRes.isPresent()) return null;
+        Product product = dbRes.get();
+        product.setProductName(newProduct.getProductName());
+        product.setProductQuantity(newProduct.getProductQuantity());
+        product.setImageLink(newProduct.getImageLink());
+        product.setProductDescription(newProduct.getProductDescription());
+
+        productRepository.save(product);
+        return product;
+    }
+    public Product deleteProduct(String productID) {
+        Product product = getProduct(productID);
+        if(product == null) return null;
+        productRepository.deleteById(productID);
+        return product;
+    }
+    public Product getProduct(String productID) {
+        Optional<Product> dbRes = productRepository.findById(productID);
+        if(!dbRes.isPresent()) return null;
+        return dbRes.get();
+    }
+    public List<Product> getAllProducts(String sellerID) {
+        List<Product> productList = productRepository.findAllProductsBySellerID(sellerID);
+        return productList;
+    } 
     
 }
